c432b7ffde57a959c5e3aba377b64d9905bdece6
Benon .M <141640190+Benon484@users.noreply.github.com>
2026-08-20 18:53:46 +0200

    backend branch

--- mainsl-student-client/src/hooks/usePeople.ts
+++ mainsl-student-client/src/hooks/usePeople.ts
@@ -7,12 +7,20 @@ export const usePeople = (personType: PersonType) => {
   const [people, setPeople] = useState<IPerson[]>([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-  //
+
+  // When naming functions:
+  // - Use camelCase
+  // - Give the function a meaningful name
+  // - The name should describe what the function does
+
+  //This function is used to fetch all the people(students and Employees) from our API.
+
   const getPeople = useCallback(async () => {
     try {
       setLoading(true);
       setError(null);
-
+      //We need to communicate with our API to retrieve the list of people.
+      //Hint Which HTTP request should we use when retrieving data?
       const response = await api.get<IPerson[]>("/people", {
         params: { personType },
       });
@@ -25,31 +33,44 @@ export const usePeople = (personType: PersonType) => {
     }
   }, [personType]);
 
-  //
+  //This function will be used to submit a new person (Student or Employee)
+
+  // Question:
+  // What would be a meaningful name for this function?
+
   const addPerson = async (person: IPerson) => {
+    // This is responsible for communicating with the API using the appropriate HTTP request.
     const { data } = await api.post<IPerson>("/people", person);
     await getPeople();
     return data;
   };
-  //
+
+  //This fuction is responsible for modifying an already existing person (Student or Employee)
+  // Question:
+  // What would be a meaningful name for this function?
+
   const updatePerson = async (person: IPerson) => {
+    // This is responsible for communicating with the API using the appropriate HTTP request.
     await api.put(`/people/${person.id}`, person);
     await getPeople();
   };
 
-  // we want to add an image
+  //This function is responsible for adding an image to a specific person (Student or Employee)
   const uploadImage = async (id: number, file: File) => {
     const formData = new FormData();
     formData.append("file", file);
 
+    // This is responsible for communicating with the API using the appropriate HTTP request.
     const { data } = await api.post<IPerson>(`/people/${id}/image`, formData);
 
     await getPeople();
     return data;
   };
 
-  //we want to remove a person from the website
+  //This Function is responsible for removing a person from the API
+
   const deletePerson = async (id: number) => {
+    // This is responsible for communicating with the API using the appropriate HTTP request.
     await api.delete(`/people/${id}`); //remove delete
     await getPeople();
   };
